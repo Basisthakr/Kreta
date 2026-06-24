3916addbe11b4188f1c1d34b61206cb463896fa8
basisthakr <basisthakum@proton.me>
2026-06-24 13:32:30 +0530

    refactor: Removed unnecessary imports
    Removed unnecessary imports from Subscription.java (import jakarta.persistence.PreUpdate;) and also added Creation Timestamp to Users.

--- src/main/java/com/basisttha/Kreta/Model/Subscription.java
+++ src/main/java/com/basisttha/Kreta/Model/Subscription.java
@@ -15,7 +15,6 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToOne;
-import jakarta.persistence.PreUpdate;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
--- src/main/java/com/basisttha/Kreta/Model/Users.java
+++ src/main/java/com/basisttha/Kreta/Model/Users.java
@@ -3,6 +3,8 @@ package com.basisttha.Kreta.Model;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+import org.hibernate.annotations.CreationTimestamp;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -10,7 +12,6 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.PrePersist;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
@@ -36,10 +37,6 @@ public class Users {
     private String stripeCustomerId;//every stripe call Kreta makes on behalf of this user uses this
     @Enumerated(EnumType.STRING)
     private Role role;
+    @CreationTimestamp
     private LocalDateTime createdAt;
-
-    @PrePersist
-    void setStuff(){
-        this.createdAt = LocalDateTime.now();
-    }
 }
