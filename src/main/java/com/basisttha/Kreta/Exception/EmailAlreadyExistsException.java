e3ea48db5b76458178dc5a8eae8e0266a2b40651
basisthakr <basisthakum@proton.me>
2026-06-24 17:36:53 +0530

    feat: add custom exceptions
    All exceptions are related to AuthService
    Exceptions - Names are self explanatory :)
    Invalid Credentials is for wrong username and password
    InvalidRefreshToken - The refresh token for getting a new JWT is invalid
    EmailAlreadyExists - Tried to use an email that already has an account, for making a new account

---
+++ src/main/java/com/basisttha/Kreta/Exception/EmailAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package com.basisttha.Kreta.Exception;
+
+public class EmailAlreadyExistsException extends RuntimeException{
+    public EmailAlreadyExistsException(String m){
+        super(m);
+    }
+}
